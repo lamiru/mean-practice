85724d7b5944b0844100b4c1f98eacbab7500862
lamiru <lamiru@gmail.com>
2015-09-10 22:29:39 +0900

    create server.js
    Command "nodemon server.js" on shell to run localhost server.

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+
+app.use('/', function(req, res) {
+    res.send('Hello World')
+})
+
+app.listen(3000)
+console.log('Server running at http://localhost:3000/')
+
+module.exports = app
