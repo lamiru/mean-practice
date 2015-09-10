f4b142a9e03c5a22785232220f9e846fa64e063a
lamiru <lamiru@gmail.com>
2015-09-10 22:43:49 +0900

    express application

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-var express = require('express')
+var express = require('./config/express')
+
 var app = express()
-
-app.use('/', function(req, res) {
-    res.send('Hello World')
-})
-
 app.listen(3000)
-console.log('Server running at http://localhost:3000/')
-
 module.exports = app
+
+console.log('Server running at http://localhost:3000/')
