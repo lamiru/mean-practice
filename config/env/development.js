0122a759617633e6208d5cbf4a53fad51479c5f5
lamiru <lamiru@gmail.com>
2015-09-11 18:33:29 +0900

    express-session

--- config/env/development.js
+++ config/env/development.js
@@ -1,3 +1,3 @@
 module.exports = {
-    // options for development env.
+    sessionSecret: 'developmentSessionSecret'
 }
--- config/express.js
+++ config/express.js
@@ -1,8 +1,10 @@
+var config = require('./config')
 var express = require('express')
 var morgan = require('morgan')
 var compress = require('compression')
 var bodyParser = require('body-parser')
 var methodOverride = require('method-override')
+var session = require('express-session')
 
 module.exports = function() {
     var app = express()
@@ -18,6 +20,12 @@ module.exports = function() {
     }))
     app.use(bodyParser.json())
     app.use(methodOverride())
+
+    app.use(session({
+        saveUninitialized: true,
+        resave: true,
+        secret: config.sessionSecret
+    }))
 
     app.set('views', './app/views')
     app.set('view engine', 'ejs')
