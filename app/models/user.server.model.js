be16968c56be17c0330916e0c42ad95d3797489b
lamiru <lamiru@gmail.com>
2015-09-19 17:37:24 +0900

    UserSchema website
    UserSchema.set('toJSON', { getters: true }) add 'http://' to its JSON
    return

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -10,10 +10,25 @@ var UserSchema = new Schema({
         trim: true
     },
     password: String,
+    website: {
+        type: String,
+        get: function(url) {
+            if (!url) {
+                return url
+            } else {
+                if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+                    url = 'http://' + url
+                }
+                return url
+            }
+        }
+    },
     created: {
         type: Date,
         default: Date.now
     }
 })
+
+UserSchema.set('toJSON', { getters: true })
 
 mongoose.model('User', UserSchema)
