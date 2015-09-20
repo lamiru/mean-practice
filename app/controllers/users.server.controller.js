b9b4279b9cbf81876ee4a488be25c0fb65811d93
lamiru <lamiru@gmail.com>
2015-09-20 18:33:27 +0900

    edit controllers, routes for authenticate

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -1,58 +1,72 @@
 var User = require('mongoose').model('User')
+var passport = require('passport')
 
-exports.create = function(req, res, next) {
-    var user = new User(req.body)
+var getErrorMessage = function(err) {
+    var message = ''
 
-    user.save(function(err) {
-        if (err) {
-            return next(err)
-        } else{
-            res.json(user)
+    if (err.code) {
+        switch (err.code) {
+            case 11000:
+            case 11001:
+                message = 'Username already exists'
+                break
+            default:
+                message = 'Something went wrong'
         }
-    })
-}
-exports.list = function(req, res, next) {
-    User.find({}, 'username email', {
-            skip: 1,
-            limit: 1
-        }, function(err, users) {
-        if (err) {
-            return next(err)
-        } else {
-            res.json(users)
+    } else {
+        for (var errName in err.errors) {
+            if (err.errors[errName].message) message = err.errors[errName].message
         }
-    })
+    }
+
+    return message
 }
-exports.read = function(req, res) {
-    res.json(req.user)
+
+
+exports.renderSignin = function(req, res, next) {
+    if (!req.user) {
+        res.render('signin', {
+            title: 'Sign-in Form',
+            messages: req.flash('error') || req.flash('info')
+        })
+    } else {
+        return res.redirect('/')
+    }
 }
-exports.userByID = function(req, res, next, id) {
-    User.findOne({
-        _id: id
-    }, function(err, user) {
-        if (err) {
-            return next(err)
-        } else {
-            req.user = user
-            next()
-        }
-    })
+exports.renderSignup = function(req, res, next) {
+    if (!req.user) {
+        res.render('signup', {
+            title: 'Sign-up Form',
+            messages: req.flash('error')
+        })
+    } else {
+        return res.redirect('/')
+    }
 }
-exports.update = function(req, res, next) {
-    User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
-        if (err) {
-            return next(err)
-        } else {
-            res.json(user)
-        }
-    })
+exports.signup = function(req, res, next) {
+    if (!req.user) {
+        var user = new User(req.body)
+        var message = null
+
+        user.provider = 'local'
+
+        user.save(function(err) {
+            if (err) {
+                var message = getErrorMessage(err)
+
+                req.flash('error', message)
+                return res.redirect('/signup')
+            }
+            req.login(user, function(err) {
+                if (err) return next(err)
+                return res.redirect('/')
+            })
+        })
+    } else {
+        return res.redirect('/')
+    }
 }
-exports.delete = function(req, res, next) {
-    req.user.remove(function(err) {
-        if (err) {
-            return next(err)
-        } else {
-            res.json(req.user)
-        }
-    })
+exports.signout = function(req, res) {
+    req.logout()
+    res.redirect('/')
 }
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,14 +1,18 @@
 var users = require('../../app/controllers/users.server.controller')
+var passport = require('passport')
 
 module.exports = function(app) {
-    app.route('/users')
-    .post(users.create)
-    .get(users.list)
+    app.route('/signup')
+    .get(users.renderSignup)
+    .post(users.signup)
 
-    app.route('/users/:userId')
-    .get(users.read)
-    .put(users.update)
-    .delete(users.delete)
+    app.route('/signin')
+    .get(users.renderSignin)
+    .post(passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/signin',
+        failureFlash: true
+    }))
 
-    app.param('userId', users.userByID)
+    app.get('/signout', users.signout)
 }
