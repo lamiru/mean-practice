c971563ee3ed42227b122eefe238cf6f154f6f89
lamiru <lamiru@gmail.com>
2015-09-21 20:47:49 +0900

    angular user

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,12 +1,6 @@
 exports.render = function(req, res) {
-    if (req.session.lastVisit) {
-        console.log(req.session.lastVisit)
-    }
-
-    req.session.lastVisit = new Date()
-
     res.render('index', {
         title: 'Hello World',
-        userFullName: req.user ? req.user.fullName : ''
+        user: JSON.stringify(req.user)
     })
 }
